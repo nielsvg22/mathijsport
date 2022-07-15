6aead7e5e61d2052e5e6e604a53eb88c4e0e4a6b
Niels Gortel <nielsgortel@nielss-mbp.home>
2022-07-15 16:11:10 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import HeroSection from './Components/HeroSection';
 import './App.css';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import Cards from './Components/Cards';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <HeroSection />
+       
+        <Cards/>
+
+
+        
+      </Router>
+    </>
   );
 }
 
